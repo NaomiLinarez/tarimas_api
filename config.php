b5afb4669b4b38378e4d9dd4efda92abad955760
NaomiLinarez <linaressnaomi@gmail.com>
2026-04-09 00:58:03 -0600

    Update config.php

--- config.php
+++ config.php
@@ -37,9 +37,3 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit; }
 
-var_dump([
-    'host' => getenv('MYSQLHOST'),
-    'port' => getenv('MYSQLPORT'),
-    'db'   => getenv('MYSQLDATABASE'),
-    'user' => getenv('MYSQLUSER'),
-]);
