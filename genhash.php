dd00445e22f6a5250077b556a2098de2debd24c1
NaomiLinarez <linaressnaomi@gmail.com>
2026-05-31 21:01:57 -0600

    Update genhash.php

--- genhash.php
+++ genhash.php
@@ -1,2 +1,6 @@
 <?php
-echo password_hash('admin1234', PASSWORD_DEFAULT);
+require_once 'config.php';
+$hash = password_hash('admin1234', PASSWORD_DEFAULT);
+$db = getDB();
+$db->prepare("UPDATE usuarios SET password_hash = ? WHERE usuario = 'admin'")->execute([$hash]);
+echo "Listo. Hash actualizado: " . $hash;
